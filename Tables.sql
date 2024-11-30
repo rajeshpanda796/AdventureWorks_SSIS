08ef8f337c6b389e64dc49c7ac87d8b0f87d01e6
rajeshpanda796 <rajeshpanda1608@gmail.com>
2024-12-01 02:25:39 +0530

    Return Package

--- Tables.sql
+++ Tables.sql
@@ -18,3 +18,32 @@ CREATE TABLE AW_ProductSubcategory(
 	CONSTRAINT FK_ProductCategoryKey FOREIGN KEY (ProductCategoryKey) 
         REFERENCES AW_ProductCategory(ProductCategoryKey) 
 )
+
+CREATE TABLE AW_Product(
+	ProductKey	INT PRIMARY KEY,
+	ProductSubcategoryKey	INT,
+	ProductSKU	VARCHAR(10),
+	ProductName	VARCHAR(35),
+	ModelName	VARCHAR(30),
+	ProductDescription	VARCHAR(250),
+	ProductColor VARCHAR(15),	
+	ProductSize	VARCHAR(5),
+	ProductStyle VARCHAR(5),
+	ProductCost	MONEY,
+	ProductPrice MONEY,
+	CONSTRAINT FK_ProductSubcategoryKey FOREIGN KEY (ProductSubcategoryKey) 
+        REFERENCES AW_ProductSubcategory(ProductSubcategoryKey) 
+)
+
+CREATE TABLE AW_Returns(
+	ReturnDate	DATE,
+	TerritoryKey INT,	
+	ProductKey	INT,
+	ReturnQuantity INT CHECK (ReturnQuantity > 0),
+    UNIQUE (ReturnDate, TerritoryKey, ProductKey),
+	CONSTRAINT FK_TerritoryKey FOREIGN KEY (TerritoryKey) 
+        REFERENCES AW_Territory(SalesTerritoryKey) , -- Don't Need LookUP
+	CONSTRAINT FK_ProductKey FOREIGN KEY (ProductKey) 
+        REFERENCES AW_Product(ProductKey) -- Don't Need LookUP
+)
+
